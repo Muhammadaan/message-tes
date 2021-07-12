dc004ca3683435ee3f2e3d735eacb920c8894885
muhammadaan <muhmmd.aan@gmail.com>
2021-07-12 10:28:20 +0700

    validasi

--- lib/controller/list_message_controller.dart
+++ lib/controller/list_message_controller.dart
@@ -22,26 +22,10 @@ class ListMessageController extends GetxController {
     loading.value = false;
   }
 
-  String readTimestamp(int timestamp) {
-    var now = new DateTime.now();
-    var format = new DateFormat('HH:mm a');
-    var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000);
-    var diff = date.difference(now);
-    var time = '';
+  String formatTimeStamp(int timestamp) {
+    var date = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    var formattedDate = DateFormat('MM dd, hh:mm a').format(date);
 
-    if (diff.inSeconds <= 0 ||
-        diff.inSeconds > 0 && diff.inMinutes == 0 ||
-        diff.inMinutes > 0 && diff.inHours == 0 ||
-        diff.inHours > 0 && diff.inDays == 0) {
-      time = format.format(date);
-    } else {
-      if (diff.inDays == 1) {
-        time = diff.inDays.toString() + 'DAY AGO';
-      } else {
-        time = diff.inDays.toString() + 'DAYS AGO';
-      }
-    }
-
-    return time;
+    return formattedDate;
   }
 }
--- lib/screen/list_message.dart
+++ lib/screen/list_message.dart
@@ -21,34 +21,103 @@ class ListMessageScreen extends StatelessWidget {
                           strokeWidth: 1,
                         )))
                       : Column(
-                          children: c.dataMessaga.map((element) {
-                          return Container(
-                            alignment: element['from'] == "A"
-                                ? Alignment.centerRight
-                                : Alignment.centerLeft,
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              margin: EdgeInsets.symmetric(vertical: 5),
-                              decoration: BoxDecoration(
-                                  color: element['from'] == "A"
-                                      ? Colors.blue[200]
-                                      : Colors.grey[200],
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: Column(
-                                crossAxisAlignment: element['from'] == "A"
-                                    ? CrossAxisAlignment.end
-                                    : CrossAxisAlignment.start,
-                                children: [
-                                  Text(element['body'] ?? "-"),
-                                  Text(c.readTimestamp(
-                                      int.parse(element['timestamp'])))
-                                ],
-                              ),
+                          children: [
+                            Text(
+                              "Pesan Gambar - Tidak Ada Judul",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
                             ),
-                          );
-                        }).toList())),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                                children: c.dataMessaga
+                                    .where((e) =>
+                                        e['attachment'] == 'image' &&
+                                        e['body'] == null)
+                                    .map((element) {
+                              return listData(element);
+                            }).toList()),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Pesan Gambar - From A",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                                children: c.dataMessaga
+                                    .where((e) =>
+                                        e['attachment'] == 'image' &&
+                                        e['from'] == 'A')
+                                    .map((element) {
+                              return listData(element);
+                            }).toList()),
+                            Text(
+                              "Pesan Gambar - From B",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                                children: c.dataMessaga
+                                    .where((e) =>
+                                        e['attachment'] == 'image' &&
+                                        e['from'] == 'B')
+                                    .map((element) {
+                              return listData(element);
+                            }).toList()),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Pesan Kontak",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                                children: c.dataMessaga
+                                    .where((e) => e['attachment'] == 'contact')
+                                    .map((element) {
+                              return listData(element);
+                            }).toList()),
+                          ],
+                        )),
             ),
           )),
+    );
+  }
+
+  Widget listData(element) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            element['body'] ?? '-',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(c.formatTimeStamp(int.parse(element['timestamp']))),
+          Text("From : ${element['from']}")
+        ],
+      ),
     );
   }
 }
